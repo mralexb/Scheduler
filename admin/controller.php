718cad6402da96e82a1c2bcaedb59cd578b74a63
James <m.james.butler@gmail.com>
2018-05-24 22:59:31 +0800

    Started controller.php. updated some of backlog. modified some CSS.

---
+++ admin/controller.php
@@ -0,0 +1,28 @@
+<?php
+
+//takes the q parameter
+$q = $_REQUEST["q"];
+
+//stores output
+$output = "";
+
+//databse info
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+if ($q == "tutor") {
+    $sql = "SELECT teacher FROM teacher";
+    $result = $conn->query($sql);
+    while ($row = $result->fetch_assoc())) {
+        echo '<option value="' .$row["teacher"]. '">' .$row["teacher"].'</option>';
+    }
+}
